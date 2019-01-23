7aa042b44e17416d024058357ada73fc621481b7
gaoyanbao <494189180@qq.com>
2019-01-23 17:27:24 +0800

    整合redis

--- src/main/java/com/example/SpringbootJpaApplicationTests.java
+++ src/main/java/com/example/SpringbootJpaApplicationTests.java
@@ -1,20 +1,30 @@
 package com.example;
 
-import com.example.entity.User;
-import com.example.repository.UserRepository;
-import org.junit.After;
-import org.junit.Before;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class SpringbootJpaApplicationTests {
-
     @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Test
+    public void Test() {
+        redisTemplate.opsForValue().set("usrname", "test");
+    }
+
+
+    /*@Autowired
     private UserRepository userRepository;
     @Test
     public void contextLoads(){
@@ -32,13 +42,35 @@ public class SpringbootJpaApplicationTests {
             userRepository.save(user);
             System.out.println(user.toString());
         });
-
     }
     //删除
     @After
     public void del(){
         userRepository.findById(2L).ifPresent(user -> userRepository.delete(user));
+    }*/
+    @Test
+    public void CsetListRedis() {
+        List<String> trap = new ArrayList<>();
+        trap.add("zs");
+        trap.add("zwj");
+        trap.add("xyjy");
+        //循环向userlist左添加值
+        trap.forEach(value -> redisTemplate.opsForList().leftPush("userlist", value));
+        //向userlist右添加值
+        redisTemplate.opsForList().rightPush("userlist", "rightValue");
+        log.info("userlist->{}", redisTemplate.opsForList().range("userlist", 0, 10));
+        //循环向userlist左添加值
+        trap.forEach(value -> redisTemplate.opsForSet().add("userset", value));
+        log.info("userlist->{}", redisTemplate.opsForSet().members("userlist"));
+
     }
+
+    @Test
+    public void DlistRedisRemove() {
+        redisTemplate.opsForList().remove("userlist", 0, "zwj");
+    }
+
+
 }
 
 
